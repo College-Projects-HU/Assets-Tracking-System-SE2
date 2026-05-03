38ac8f140a37ee8bdcb5fbe7c5d50517c83e8719
MalkGaafar <malk.gaafar04@gmail.com>
2026-05-03 18:13:15 +0300

    add new asset service

---
+++ asset-service/src/main/java/assetservice/exception/IllegalStateTransitionException.java
@@ -0,0 +1,5 @@
+package com.assets.assetservice.exception;
+
+public class IllegalStateTransitionException extends RuntimeException {
+    public IllegalStateTransitionException(String msg) { super(msg); }
+}
---
+++ asset-service/src/main/java/assetservice/exception/NotFoundException.java
@@ -0,0 +1,5 @@
+package com.assets.assetservice.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String msg) { super(msg); }
+}
