3651ebaf71c41aab46a09f34c3e4268a4f2b4100
abdunader <abdulrahman_20230326@fci.helwan.edu.eg>
2026-05-08 00:39:05 +0300

    refactor: parameterize service configurations and environment variables for improved service discovery and modular deployment

--- Backend/init-db.sql
+++ Backend/init-db.sql
@@ -1,5 +1,6 @@
-CREATE DATABASE assets_auth_db;
-CREATE DATABASE assets_asset_db;
-CREATE DATABASE assets_user_db;
-CREATE DATABASE assets_report_db;
-CREATE DATABASE assets_maintenance_db;
+SELECT 'CREATE DATABASE assets_auth_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'assets_auth_db')\gexec
+SELECT 'CREATE DATABASE assets_asset_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'assets_asset_db')\gexec
+SELECT 'CREATE DATABASE assets_user_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'assets_user_db')\gexec
+SELECT 'CREATE DATABASE assets_report_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'assets_report_db')\gexec
+SELECT 'CREATE DATABASE assets_maintenance_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'assets_maintenance_db')\gexec
+SELECT 'CREATE DATABASE assets_notification_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'assets_notification_db')\gexec
